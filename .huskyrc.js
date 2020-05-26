b3fe519d3484cddf30d1b2697df5e7398dc85da4
Daniele Belardi <dwon.dnl@gmail.com>
2020-05-26 07:12:39 +0200

    chore(package): update deps
    + standard@14.3.4
    + lint-staged@10.2.6
    + husky@4.2.5

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   hooks: {
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
     'pre-commit': 'lint-staged',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
