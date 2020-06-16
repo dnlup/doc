a17342aff9c4caf37329a477505b408b3c341981
Daniele Belardi <dwon.dnl@gmail.com>
2020-06-16 08:59:33 +0200

    build: restore lint-staged
    Without lint-staged the hooks pipeline dies not work as expected.

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,12 +1,7 @@
 'use strict'
 
-const tasks = arr => arr.join(' && ')
-
 module.exports = {
   hooks: {
-    'pre-commit': tasks([
-      'npm run lint',
-      'npm run doc'
-    ])
+    'pre-commit': 'lint-staged'
   }
 }
