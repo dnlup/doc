c7cb7b66d36ed8bc14bf803a80bb29a9236f64a0
acheronfail <acheronfail@gmail.com>
2020-06-29 21:02:08 +1000

    feat: add TypeScript typings file

---
+++ index.d.ts
@@ -0,0 +1,92 @@
+declare module '@dnlup/doc' {
+  export interface DocOptions {
+    /**
+     * Sample interval (ms), each sampleInterval ms a data event is emitted.
+     * On Node 10 the default value is 500 while on Node >= 12 is 1000.
+     * Under the hood the package uses monitorEventLoopDelay where available to
+     * sample the event loop and this allows to increase the default sample
+     * interval on Node >= 12.
+     */
+    sampleInterval?: number;
+
+    /**
+     * Options to setup `perf_hooks.monitorEventLoopDelay`.
+     */
+    eventLoopOptions?: {
+      /**
+       * The sampling rate in milliseconds. Must be greater than zero. Default: 10.
+       */
+      resolution: number;
+    };
+  }
+
+  /**
+   * On Node 12 and above this is a Histogram instance from 'perf_hooks'.
+   */
+  interface EventLoopDelayHistogram {
+    min: number;
+    max: number;
+    mean: number;
+    stddev: number;
+    percentiles: Map<number, number>;
+    exceeds: number;
+  }
+
+  export interface DocData {
+    /**
+     * Event loop delay (ms).
+     */
+    eventLoopDelay: number;
+    /**
+     * Object containing memory usage stats.
+     */
+    memory: {
+      /**
+       * RSS memory (bytes).
+       */
+      rss: number;
+      /**
+       * Total heap Memory (bytes).
+       */
+      heapTotal: number;
+      /**
+       * Heap memory used (bytes).
+       */
+      heapUsed: number;
+      /**
+       * External memory (bytes).
+       */
+      external: number;
+    };
+    /**
+     * CPU usage percentage.
+     */
+    cpu: number;
+    /**
+     * Object containing raw values
+     */
+    raw: {
+      /**
+       * Raw representation of the event loop delay, on Node 10 it is the delay
+       * in nanoseconds, on Node >= 12 is a Histogram instance
+       */
+      eventLoopDelay: number | EventLoopDelayHistogram;
+      /**
+       * Object containing the raw values returned from `process.cpuUsage()`.
+       */
+      cpu: {
+        user: number;
+        system: number;
+      };
+    };
+  }
+
+  import { EventEmitter } from 'events';
+
+  export interface DocInstance extends EventEmitter {
+    on(event: 'data', listener: (data: DocData) => void);
+  }
+
+  declare function NewDoc(options?: DocOptions): DocInstance;
+  export = NewDoc;
+}
