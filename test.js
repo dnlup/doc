1d2f8c59c00207dcf8ddbb18f37bd120ab8fe1d9
Daniele Belardi <dwon.dnl@gmail.com>
2020-07-13 08:10:01 +0200

    test: add more detailed messages remaining checks

--- test.js
+++ test.js
@@ -10,50 +10,111 @@ const nodeMajorVersion = parseInt(process.versions.node.split('.')[0])
 const performDetailedCheck = process.platform === 'linux' || !isCi
 
 const checks = {
-  eventLoopDelay (value) {
+  eventLoopDelay (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
       const level = nodeMajorVersion >= 12 ? 1 : 20
-      return value > 0 && value < level
+      check = value > 0 && value < level
+      expected = `0 < x < ${level}`
+    } else {
+      check = value >= 0
+      expected = 'x >= 0'
     }
-    return value >= 0
+    t.true(check, `eventLoopDelay | expected: ${expected}, value: ${value}`)
   },
-  cpu (value) {
+  cpu (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
-      return value > 0 && value < 30
+      check = value > 0 && value < 30
+      expected = '0 < x < 30'
+    } else {
+      // Apparently, sometimes cpu usage is zero on windows runners.
+      check = value >= 0
+      expected = 'x >= 0'
     }
-    // Apparently, sometimes cpu usage is zero on windows runners.
-    return value >= 0
+    t.true(check, `cpu | expected: ${expected}, value: ${value}`)
   },
-  rss (value) {
+  rss (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
-      const level = nodeMajorVersion >= 12 ? 130 : 150
-      return value > 0 && value < level * 1e6
+      const level = (nodeMajorVersion >= 12 ? 130 : 150) * 1e6
+      check = value > 0 && value < level
+      expected = `0 < x < ${level}`
+    } else {
+      check = value > 0
+      expected = 'x > 0'
     }
-    return value > 0
+    t.true(check, `rss | expected: ${expected}, value: ${value}`)
   },
-  heapTotal (value) {
+  heapTotal (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
-      const level = nodeMajorVersion >= 12 ? 100 : 120
-      return value > 0 && value < level * 1e6
+      const level = (nodeMajorVersion >= 12 ? 100 : 120) * 1e6
+      check = value > 0 && value < level
+      expected = `0 < x < ${level}`
+    } else {
+      check = value > 0
+      expected = 'x > 0'
     }
-    return value > 0
+    t.true(check, `heapTotal | expected: ${expected}, value: ${value}`)
   },
-  heapUsed (value) {
+  heapUsed (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
-      const level = nodeMajorVersion >= 12 ? 80 : 100
-      return value > 0 && value < level * 1e6
+      const level = (nodeMajorVersion >= 12 ? 80 : 100) * 1e6
+      check = value > 0 && value < level
+      expected = `0 < x < ${level}`
+    } else {
+      check = value > 0
+      expected = 'x > 0'
     }
-    return value > 0
+    t.true(check, `heapUsed | expected: ${expected}, value: ${value}`)
   },
-  external (value) {
+  external (t, value) {
+    let check
+    let expected
     if (performDetailedCheck) {
-      return value > 0 && value < 10 * 1e6
+      const level = 10 * 1e6
+      check = value > 0 && value < level
+      expected = `0 < x < ${level}`
+    } else {
+      check = value > 0
+      expected = 'x > 0'
     }
-    return value > 0
+    t.true(check, `external | expected: ${expected}, value: ${value}`)
   },
-  gc (value) {
+  gc (t, value) {
+    const levels = {
+      major: 0,
+      minor: 0,
+      incremental: 0
+    }
+    const expected = {
+      major: `x > ${levels.major}`,
+      minor: `x > ${levels.minor}`,
+      incremental: `x > ${levels.incremental}`
+    }
+
     // Not sure how to deterministically trigger a WeakCB GC cycle, so we don't check it here
-    return value.major > 0 && value.minor > 0 && value.incremental > 0
+    const check = value.major > levels.major &&
+                  value.minor > levels.minor &&
+                  value.incremental > levels.incremental
+    t.true(check, `gc |
+    expected: {
+      major: ${expected.major},
+      minor: ${expected.minor},
+      incremental: ${expected.incremental}
+    },
+    value: { 
+      major: ${value.major},
+      minor: ${value.minor},
+      incremental: ${value.incremental}
+    }`)
   }
 }
 
@@ -106,12 +167,12 @@ tap.test('data event', t => {
     t.equal('number', typeof data.memory.heapTotal)
     t.equal('number', typeof data.memory.heapUsed)
     t.equal('number', typeof data.memory.external)
-    t.true(checks.eventLoopDelay(data.eventLoopDelay), `event loop delay check: ${data.eventLoopDelay}`)
-    t.true(checks.cpu(data.cpu), `cpu check: ${data.cpu}`)
-    t.true(checks.rss(data.memory.rss), `rss check: ${data.memory.rss}`)
-    t.true(checks.heapTotal(data.memory.heapTotal), `heapTotal check: ${data.memory.heapTotal}`)
-    t.true(checks.heapUsed(data.memory.heapUsed), `heapUsed check: ${data.memory.heapUsed}`)
-    t.true(checks.external(data.memory.external), `external check: ${data.memory.external}`)
+    checks.eventLoopDelay(t, data.eventLoopDelay)
+    checks.cpu(t, data.cpu)
+    checks.rss(t, data.memory.rss)
+    checks.heapTotal(t, data.memory.heapTotal)
+    checks.heapUsed(t, data.memory.heapUsed)
+    checks.external(t, data.memory.external)
     t.end()
   })
 })
