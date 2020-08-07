b2663d1cc351a1484b91fc04037ad10b4584bc86
Daniele Belardi <dwon.dnl@gmail.com>
2020-08-07 15:51:10 +0200

    chore: fix lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,6 @@ module.exports = {
     const list = filenames.map(filename => `'markdown-toc -i ${filename}`)
     return list
   },
-  '*.{js}': ['standard --fix'],
-  '*.{d.ts}': ['standard --parser @typescript-eslint/parser --plugin @typescript-eslint/eslint-plugin --fix']
+  '*.js': ['standard --fix'],
+  '*.d.ts': ['standard --parser @typescript-eslint/parser --plugin @typescript-eslint/eslint-plugin --fix']
 }
