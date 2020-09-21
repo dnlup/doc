8e15c36a28dacafb29e0a2e83f6757c658963cc0
Daniele Belardi <dwon.dnl@gmail.com>
2020-09-21 11:12:31 +0200

    build: use eslint for linting js and ts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,5 @@ module.exports = {
     const list = filenames.map(filename => `'markdown-toc -i ${filename}`)
     return list
   },
-  '*.js': ['standard --fix'],
-  '*.d.ts': ['standard --parser @typescript-eslint/parser --plugin @typescript-eslint/eslint-plugin --fix']
+  '*.{d.ts,js}': ['eslint --fix']
 }
