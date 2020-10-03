b143d153b1df55d4e32770696491b2e7b5c31205
Daniele Belardi <dwon.dnl@gmail.com>
2020-10-03 18:18:40 +0200

    fix(config): drop ajv-cli and build step
    Implement config validation without using JSON schema.
    
    BREAKING CHANGE: message errors and instances are changed.

--- test/doc.test.js
+++ test/doc.test.js
@@ -80,7 +80,7 @@ const checks = {
     let check
     let expected
     if (performDetailedCheck) {
-      const level = 10 * 11e5
+      const level = 10 * 15e5
       check = value > 0 && value < level
       expected = `0 < x < ${level}`
     } else {
@@ -131,27 +131,6 @@ function preventTestExitingEarly (t, ms) {
   const timeout = setTimeout(() => {}, ms)
   t.teardown(() => clearTimeout(timeout))
 }
-
-tap.test('should throw an error if options are invalid', t => {
-  let error = t.throws(() => doc({ sampleInterval: 'skdjfh' }))
-  t.equal(error.message, '.sampleInterval should be number')
-  error = t.throws(() => doc({ sampleInterval: -1 }))
-  t.equal(error.message, '.sampleInterval should be >= 1')
-  if (monitorEventLoopDelay) {
-    error = t.throws(() => doc({ sampleInterval: 5 }))
-    t.equal('.sampleInterval should be >= .eventLoopOptions.resolution',
-      error.message)
-    error = t.throws(() => doc({
-      sampleInterval: 10,
-      eventLoopOptions: { resolution: 20 }
-    }))
-    t.equal('.sampleInterval should be >= .eventLoopOptions.resolution',
-      error.message)
-  } else {
-    t.pass()
-  }
-  t.end()
-})
 
 tap.test('sample', t => {
   const start = process.hrtime()
