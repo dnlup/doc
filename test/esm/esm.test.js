cc4d258375c30bc84c792f2f4a5a361222012e54
Daniele Belardi <dwon.dnl@gmail.com>
2021-03-16 10:49:47 +0100

    test(esm): add named exports test

--- test/esm/esm.test.js
+++ test/esm/esm.test.js
@@ -4,9 +4,20 @@ const { join } = require('path')
 
 const nodeMajorVersion = parseInt(process.versions.node.split('.')[0])
 
-test('sample', { skip: nodeMajorVersion <= 11 }, t => {
+test('self resolution', { skip: nodeMajorVersion <= 11 }, t => {
   try {
-    const file = join(__dirname, 'esm.mjs')
+    const file = join(__dirname, 'selfResolution.mjs')
+    execSync(`node ${file}`)
+  } catch (e) {
+    t.error(e)
+  } finally {
+    t.end()
+  }
+})
+
+test('named exports', { skip: nodeMajorVersion <= 11 }, t => {
+  try {
+    const file = join(__dirname, 'namedExports.mjs')
     execSync(`node ${file}`)
   } catch (e) {
     t.error(e)
