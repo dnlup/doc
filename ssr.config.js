5297a986a324b827cbe614567e9470e91314f41e
Daniele Belardi <dwon.dnl@gmail.com>
2021-04-01 19:12:22 +0200

    wip: deploy on gcp appengine

---
+++ ssr.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    name: '@dnlup/doc',
+    repo: 'https://github.com/dnlup/doc',
+    auto2top: true,
+    loadSidebar: true,
+    subMaxLevel: 3,
+    maxLevel: 4
+  }
+}
