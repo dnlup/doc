410bbf0c19634a971316586e4ab460d9e9717125
dnlup <dnlup.dev@gmail.com>
2023-09-22 13:54:59 +0200

    fix(types): fix exported types

--- types/resourceUsageMetric.d.ts
+++ types/resourceUsageMetric.d.ts
@@ -1,3 +1,7 @@
+import { resourceUsage } from "process";
+
+type ResourceUsage = ReturnType<typeof resourceUsage>
+
 export interface ResourceUsageMetric {
   /**
     * Cpu usage percentage
@@ -6,22 +10,5 @@ export interface ResourceUsageMetric {
    /**
      * Raw vaule returned by `process.resourceUsage()`
      */
-   raw: {
-     fsRead: number;
-     fsWrite: number;
-     involuntaryContextSwitches: number;
-     ipcReceived: number;
-     ipcSent: number;
-     majorPageFault: number;
-     maxRSS: number;
-     minorPageFault: number;
-     sharedMemorySize: number;
-     signalsCount: number;
-     swappedOut: number;
-     systemCPUTime: number;
-     unsharedDataSize: number;
-     unsharedStackSize: number;
-     userCPUTime: number;
-     voluntaryContextSwitches: number;
-   }
+   raw: ResourceUsage
 }
